0c810d4e0b6d6e5a3268142fd2f0e6c15917e403
Charlie Dil <40442056+charliedil@users.noreply.github.com>
2023-08-31 14:52:12 -0400

    Update hunk_viewer.py
    Something happened and my changes were gone

--- hunk_viewer.py
+++ hunk_viewer.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pickle
+import time
 
 def increment_index(maxindex):
     if st.session_state.cveindex+1<=maxindex:
@@ -18,8 +19,10 @@ def write_ans(cve_to_index,ans,reasons,maxindex):
 def jump_index(jumpnum,maxindex):
     if jumpnum<=maxindex:
         st.session_state.cveindex=jumpnum
+#        st.experimental_rerun()
     else:
         st.toast("Index you are trying to jump to does not exist :(")
+        time.sleep(1)
 hunks = []
 with open("dumps/hunks_array.pkl","rb") as f:
     hunks = pickle.load(f)
@@ -34,17 +37,19 @@ with open("dumps/cve_array.pkl","rb") as f:
     cve = pickle.load(f)
 cve_to_index = []
 thing = [cve_to_index.append(c) for c in cve if c not in cve_to_index]
-maxindex = len(cve_to_index)
+maxindex = len(cve_to_index)-1
+print(maxindex)
 if "cveindex" not in st.session_state:
     st.session_state.cveindex = 0
 page = st.empty()
 with open("annotation.txt") as f:
     st.download_button("Download CSV",f)
+with st.expander("Jump to..."):
+    st.markdown("Current Index/Primary key (starts from ZERO): "+str(st.session_state.cveindex))
+    jumpnum = st.text_input("Jump to what index","0")
+    button2 = st.button("Jump now!",on_click=jump_index,args=(int(jumpnum),maxindex,))
 with page.container():
-    with st.expander("Jump to..."):
-        st.markdown("Current Index/Primary key (starts from ZERO): "+str(st.session_state.cveindex))
-        jumpnum = st.text_input("Jump to what index","0")
-        button2 = st.button("Jump now!",on_click=jump_index,args=(int(jumpnum),maxindex,cve_to_index,cve,))
+
     with st.form("my form"):
         #st.title("Index/Primary key (starts from ZERO): "+str(st.session_state.cveindex))
         st.title("Hunk viewer for annotators")
@@ -57,9 +62,9 @@ with page.container():
         ans = []
         reasons = []
         for i in indices:
-            st.markdown("Hunk "+str(i))
+            st.markdown("**Hunk "+str(i)+"**")
             st.code(hunks[i])
-            ans.append(st.radio("Is it relevant?", ["yes", "no"], key=i))
+            ans.append(st.radio("Is it relevant to fixing the vulnerability?", ["yes", "no"], key=i))
             reasons.append(st.text_input("Reasoning if no","Type here", key=10000-i))
         submitted = st.form_submit_button("Submit")#,on_click=write_ans, args=(cve_to_index,ans,reasons,maxindex,))
         if submitted:
